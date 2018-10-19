285992b6c6a4d8f96d0efd872ecb7ee6b60a9af4
ostormer <oskar.stormer@gmail.com>
2018-10-19 14:33:31 +0200

    la til agent_do_actrion()

--- KPC_agent.py
+++ KPC_agent.py
@@ -33,9 +33,9 @@ class KPC_agent():
             signal = self.keypad.get_next_signal()
 
         return signal
-    
-    #a3 - a5 om passord er rett, a4 hvis feil
-    # verifisere login 
+
+    # a3 om passord er rett, a4 hvis feil
+    # verifisere login
     def verify_login(self):
         if self.current_password == self.passcode_buffer:
             self.override_signal = "Y"
@@ -43,20 +43,14 @@ class KPC_agent():
         else:
             self.override_signal = "N"
             self.flash_leds(dur, flash_dur)  # skrev feil passord, flashe leds
-    
-    #a4
-    def wrong_password_reset(self):
-        #hvis det er feil passord, den er ikke logget inn
-        #eller mens du skriver passord og trykker inn en # 
-        pass
-    
-    #a6
+
+    # a4, a5, a6
     def reset_agent_attributes(self):
-        #reset dur, lid, og muligens passcode buffer
+        # reset dur, lid, og muligens passcode buffer
         return
-    
+
     # validere passordendring
-    #a11
+    # a12
     def validate_passcode_change(self):
         if len(self.passcode_buffer) > 3 and self.passcode_buffer.isdigit():
             self.twinkle_leds(3, 0.1)
@@ -70,28 +64,28 @@ class KPC_agent():
         f.write(self.passcode_buffer)
         f.close()
         self.current_password = self.passcode_buffer
-    
-    
-    # adde en char til passcode_buffer 
-    #a2
+
+    # adde en char til passcode_buffer
+    # a2
     def add_to_passcode_buffer(self, sig: str):
         self.passcode_buffer += sig
 
     # obs! usikker på duration og hvordan det tolkes videre
-    
-    #a7
+
+    # a7
     def set_led_id(self, id: str):
         self.lid = int(id)
-    #a8
+    # a8
+
     def begin_dur_entry(self):
         self.ldur = ""
 
-    #a9
-    def append_dur_digit(self, dur:str):
+    # a9
+    def append_dur_digit(self, dur: str):
         self.ldur += dur
 
-    #a10    
-    def light_the_led(self):
+    # a10
+    def light_selected_led(self):
         dur = int(self.ldur)
         self.light_one_led(self.lid, dur)
 
@@ -112,6 +106,26 @@ class KPC_agent():
 
     def agent_do_action(self, sig):
         if sig == "A1":
-            
-            
-        pass
+            self.init_passcode_entry()
+        elif sig == "A2":
+            self.add_to_passcode_buffer()
+        elif sig == "A3":
+            self.verify_login()
+        elif sig == "A4":
+            self.reset_agent_attributes()
+        elif sig == "A5":
+            self.reset_agent_attributes()
+        elif sig == "A6":
+            self.reset_agent_attributes()
+        elif sig == "A7":
+            self.set_led_id()
+        elif sig == "A8":
+            self.begin_dur_entry()
+        elif sig == "A9":
+            self.append_dur_digit()
+        elif sig == "A10":
+            self.light_selected_led()
+        elif sig == "A11":
+            self.reset_agent_attributes()
+        elif sig == "A12":
+            self.validate_passcode_change()
