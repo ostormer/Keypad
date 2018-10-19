321f3ebbc52e38814369db0ec8b344ad5f4e567b
sglzf <36081915+sglzf@users.noreply.github.com>
2018-10-19 13:28:27 +0200

    Update KPC_agent.py

--- KPC_agent.py
+++ KPC_agent.py
@@ -15,10 +15,10 @@ class KPC_agent():
         file.close()
 
         self.override_signal = ""
-        self.lid = 0   # led id
-        self.ldur = None  # led duration
+        self.lid = None   # led id
+        self.ldur = ""  # led duration
 
-    # ready to receive password
+    # ready to receive password a1
     def init_passcode_entry(self):
         self.passcode_buffer = ""
         self.ledboard.power_up()
@@ -33,8 +33,9 @@ class KPC_agent():
             signal = self.keypad.get_next_signal()
 
         return signal
-
-    # verifisere login
+    
+    #a3 - a5 om passord er rett, a4 hvis feil
+    # verifisere login 
     def verify_login(self):
         if self.current_password == self.passcode_buffer:
             self.override_signal = "Y"
@@ -42,7 +43,20 @@ class KPC_agent():
         else:
             self.override_signal = "N"
             self.flash_leds(dur, flash_dur)  # skrev feil passord, flashe leds
-
+    
+    #a4
+    def wrong_password_reset(self):
+        #hvis det er feil passord, den er ikke logget inn
+        #eller mens du skriver passord og trykker inn en # 
+        pass
+    
+    #a6
+    def reset_agent_attributes(self):
+        #reset dur, lid, og muligens passcode buffer
+        return
+    
+    
+    
     # validere passordendring
     def validate_passcode_change(self):
         if len(self.passcode_buffer) > 3 and self.passcode_buffer.isdigit():
@@ -55,19 +69,31 @@ class KPC_agent():
         else:
             self.flash_leds(3, 0.3)
 
-    # adde en char til passcode_buffer
+    # adde en char til passcode_buffer 
+    #a2
     def add_to_passcode_buffer(self, sig: str):
         self.passcode_buffer += sig
 
     # obs! usikker på duration og hvordan det tolkes videre
+    
+    #a7
     def set_led_id(self, id: str):
         self.lid = int(id)
+    #a8
+    def begin_dur_entry(self):
+        self.ldur = ""
 
-    def set_ldur(self, dur: str):
-        self.ldur = int(dur)
+    #a9
+    def append_dur_digit(self, dur:str):
+        self.ldur += dur
 
-    def light_one_led(self, led):
-        self.ledboard.light_led(led)  # ldur og lid
+    #a10    
+    def light_the_led(self):
+        dur = int(self.ldur)
+        self.light_one_led(self.lid, dur)
+
+    def light_one_led(self, led_id, dur):
+        self.ledboard.light_led(led_id, dur)  # ldur og lid
 
     def flash_leds(self, dur, flash_dur):
         self.ledboard.flash_all_leds(dur, flash_dur)
